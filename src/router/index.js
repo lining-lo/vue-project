16c6d8c15dfc9edc4d5cf1804c4b6beaa0495ae8
lining <lo@qq.com>
2025-03-17 20:04:08 +0800

    重写push|replace方法解决连续跳转同一路由报错问题

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,27 @@ import PageTwo from '../views/PageTwo'
 
 Vue.use(VueRouter)
 
+//重写push|replace方法解决连续跳转同一路由报错问题
+//第一个参数：要跳转的路径
+//第二个参数：成功的回调
+//第三个参数：失败的回调
+const originPush = VueRouter.prototype.push
+const originReplace = VueRouter.prototype.replace
+VueRouter.prototype.push = function(lacation,resolve,reject){
+    if(resolve&&reject){
+        originPush.call(this,location,resolve,reject)
+    }else{
+        originPush.call(this,lacation,()=>{},()=>{})
+    }
+}
+VueRouter.prototype.replace = function(location,resolve,reject){
+    if (resolve&&reject) {
+        originReplace.call(this,location,resolve,reject)
+    }else{
+        originReplace.call(this,location,()=>{},()=>{})
+    }
+}
+
 export default new VueRouter({
     routes:[
         {
