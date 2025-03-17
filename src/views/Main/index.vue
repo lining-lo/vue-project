80226bd5d2c24de6e2a6c0a7dcd01ff42d19a9ee
lining <lo@qq.com>
2025-03-17 09:00:37 +0800

    整体UI的搭建

--- src/views/Main/index.vue
+++ src/views/Main/index.vue
@@ -1,16 +1,22 @@
 <template>
     <div>
-        我是Main
-        <router-view></router-view>
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-container>
+                <el-header>Header</el-header>
+                <el-main>
+                    <router-view></router-view>
+                </el-main>
+            </el-container>
+        </el-container>
+
     </div>
-  </template>
-  
-  <script>
-      export default {
-          name:'Main'
-      }
-  </script>
-  
-  <style>
-  
-  </style>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style></style>
