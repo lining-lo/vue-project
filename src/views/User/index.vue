71c010a15213ba767868695c56a041c2f4e74ffb
lining <lo@qq.com>
2025-03-20 21:02:48 +0800

    显示用户列表功能的实现

--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="manage">
-        <el-button type="primary" @click="dialogVisible = true">+新增</el-button>
         <!-- dialog提示框 -->
         <el-dialog title="提示" :before-close="handleClose" :visible.sync="dialogVisible" width="50%">
             <!-- form表单 -->
@@ -31,6 +30,27 @@
                 <el-button type="primary" @click="submit">确 定</el-button>
             </span>
         </el-dialog>
+        <div class="manage-header">
+            <el-button type="primary" @click="dialogVisible = true">+新增</el-button>
+            <!-- 用户列表表格 -->
+            <el-table :data="tableData" style="width: 100%">
+                <el-table-column prop="name" label="姓名"></el-table-column>
+                <el-table-column prop="age" label="年龄"></el-table-column>
+                <el-table-column prop="sex" label="性别">
+                    <template slot-scope="scope">
+                        <span style="margin-left: 10px;">{{ scope.row.sex == 1 ? '男' : '女' }}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="birth" label="出生日期"></el-table-column>
+                <el-table-column prop="addr" label="地址"></el-table-column>
+                <el-table-column label="操作">
+                    <template slot-scope="scope">
+                        <el-button size="mini" @click="handleEdit(scope.row)">编辑</el-button>
+                        <el-button size="mini" type="danger" @click="handleDelete(scope.row)">删除</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
     </div>
 </template>
 
@@ -40,7 +60,9 @@ export default {
     name: 'User',
     data() {
         return {
+            //控制弹出框显示与隐藏
             dialogVisible: false,
+            //新增用户表单数据
             form: {
                 name: '',
                 age: '',
@@ -48,13 +70,16 @@ export default {
                 birth: '',
                 addr: ''
             },
+            //新增用户表单校验规则
             rules: {
                 name: [{ required: true, message: '请输入姓名' }],
                 age: [{ required: true, message: '请输入年龄' }],
                 sex: [{ required: true, message: '请选择性别' }],
                 birth: [{ required: true, message: '请选择日期' }],
                 addr: [{ required: true, message: '请输入地址' }]
-            }
+            },
+            //用户列表数据
+            tableData: []
         };
     },
     methods: {
@@ -79,11 +104,21 @@ export default {
             this.$refs.form.resetFields()
             //关闭弹窗
             this.dialogVisible = false
+        },
+        //修改用户的方法
+        handleEdit(row) {
+            console.log('修改用户的方法');
+        },
+        //删除用户的方法
+        handleDelete(row) {
+            console.log('删除用户的方法');
         }
     },
     mounted() {
-        console.log(getUserList());
-        
+        getUserList().then(({ data }) => {
+            //取出获取用户列表的数据
+            this.tableData = data.list
+        })
     }
 }
 </script>
